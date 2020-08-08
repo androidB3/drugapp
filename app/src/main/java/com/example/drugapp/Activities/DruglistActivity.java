9b63e5c562929418d9e0d1bb1405e27d6e628274
Doan Duy Thuc <duythuc4444@gmail.com>
2020-08-08 16:07:28 +0700

    ThemGD

--- app/src/main/java/com/example/drugapp/Activities/DruglistActivity.java
+++ app/src/main/java/com/example/drugapp/Activities/DruglistActivity.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 public class DruglistActivity extends AppCompatActivity {
     private ArrayList<Drug> drugArrayList;
 
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
