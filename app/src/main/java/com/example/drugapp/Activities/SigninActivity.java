98418505a3ad169525b81d3c73fc5b0ad3f52b52
ivermin1123 <mrkjin99@gmail.com>
2020-08-20 18:00:17 +0700

    change Singin Screen

---
+++ app/src/main/java/com/example/drugapp/Activities/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.drugapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drugapp.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
